eb7e44546db04f33abfa945d65c8795516277b21
Teodor Radu <teodor.rd27@gmail.com>
2025-01-24 23:07:59 +0000

    Introduce zod validation and querystring typing - Use robust URL validator (built-in zod url validator not good enough)

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,50 @@
 import Fastify from 'fastify'
+import { validatorCompiler, ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import isURL from 'validator/lib/isURL'
 
 const buildFastify = () => {
   const fastify = Fastify({
     logger: true,
   })
 
-  fastify.get('/health', (req, res) => {
-    res.send(JSON.stringify({
+  fastify.setValidatorCompiler(validatorCompiler)
+
+  // SECURITY: custom handler prevents sensitive Fastify information from being exposed
+  // TODO: move to separate module
+  fastify.setErrorHandler((error, _, reply) => {
+    // TODO: must improve message format
+    if (error) {
+      return reply
+        .status(error.statusCode || 400)
+        .send({
+          statusCode: error.statusCode || 400,
+          error: 'Bad Request',
+          message: error.message,
+        })
+    }
+    return reply.send(error)
+  })
+
+  fastify.get('/health', { schema: { querystring: z.strictObject({}) }}, (_, res) => {
+    const payload = {
       status: 'ok',
-    }))
+    }
+    res.send(payload)
+  })
+
+  fastify.withTypeProvider<ZodTypeProvider>().get('/encode', { schema: {
+    querystring: z.strictObject({
+      url: z.string({
+        required_error: '\'url\' query is required. Example: http://localhost:3000/encode?url=https://example.com',
+      }).refine((url) => {
+        return isURL(url, { protocols: ['https'], allow_query_components: false })
+      }, {
+        message: 'Invalid URL. Hint: Make sure to use the HTTPS protocol. For URLs with query parameters, use POST endpoint instead',
+      }),
+    }),
+  } }, (req, res) => {
+    res.send(req.query)
   })
 
   return fastify
